d335413921ef55137d6d786eedb039ee80205807
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-10 18:46:07 +0530

    Add logic for sumAll excercise-formula based

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let result = 'ERROR';
+    //To check if num1,num2 are of type numbers and their value is  greater than 0
+    if (typeof num1 === 'number' && typeof num2 === 'number' && (num1 > 0 && num2 > 0)) {
+        const resultFunction = (a, b) => (b * (b + a)) / 2;
 
+        if (num1 > num2) {
+            result = resultFunction(num2, num1);
+        } else if (num1 < num2) {
+            result = resultFunction(num1, num2);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
