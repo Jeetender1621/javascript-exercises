4afc92f144fc7c051b5ddcccc0ca7d1f715df417
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-10 16:07:37 +0530

    Add code for 03_reverseString (Decrementing For Loop)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let stringReverse = "";
+    if (string !== null && string !== undefined) {
+        for (let i = string.length - 1; i >= 0; i--) {
+            stringReverse += string[i];
+        }
+    }
+    else{
+        return 'ERROR'
+    }
 
+    return stringReverse;
 };
+//can also use 
+/*
+return string.split("").reverse().join("");
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
