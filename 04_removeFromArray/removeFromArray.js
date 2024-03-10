d1e44b290690f5418dea8eaef01831ddbddf1317
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-10 16:51:02 +0530

    Add logic for removes a single value test case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,value) {
+   for(let pos = 0; pos<arr.length; pos++){
+     if(arr[pos] === value){
+        arr.splice(pos,1);
+        break;
+     }
+   }
 
+   return arr;
 };
 
 // Do not edit below this line
