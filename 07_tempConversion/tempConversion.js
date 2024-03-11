6d9bb030f4d3a03e5007a2716e8e01eb370d9339
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-11 22:47:49 +0530

    Add solution for tempConversion excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = +((((tempFahrenheit - 32)* 5/9)*100)/100).toFixed(1);
+  return tempCelsius;  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = +((((9/5 * tempCelsius) + 32)*100)/100).toFixed(1);
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
