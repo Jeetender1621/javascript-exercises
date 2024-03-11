f1665a3b74cd2d444bf345bdd7f21fd485345328
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-11 22:13:30 +0530

    Add solution for leapYears excercise for all test cases

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+   //leap year should be divisible by 4
+   if(year%4 === 0){
+    //years divisible by 100 are not leap years unless they are divisible by 400
+    if(year%100 === 0 && year%400 !== 0){
+        return false;
+    }
+    else if(year%400 === 0){
+        return true;
+    }
+    return true;
+   }
+   else return false;
 };
 
 // Do not edit below this line
