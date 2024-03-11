582480a0c5462a048952270e7105f1c65fdc2075
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-11 22:18:09 +0530

    Modify logic(leapYear) for a much more simplified approach

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,6 @@
 const leapYears = function(year) {
-   //leap year should be divisible by 4
-   if(year%4 === 0){
-    //years divisible by 100 are not leap years unless they are divisible by 400
-    if(year%100 === 0 && year%400 !== 0){
-        return false;
-    }
-    else if(year%400 === 0){
-        return true;
-    }
-    return true;
-   }
-   else return false;
+   //Simplied logic for leapyear check
+   return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
 };
 
 // Do not edit below this line
