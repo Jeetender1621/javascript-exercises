25a0a319168756efaebffa8676214d7c438f3e0b
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-20 18:03:44 +0530

    Add logic till 'multiplies several numbers' test case

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,22 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  if(num2 > num1){
+	  let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce( (sum,arrayValue) => sum+arrayValue,0);
 };
 
-const multiply = function() {
-
+const multiply = function(mulArray) {
+  return mulArray.reduce( (multiply,arrayValue) => multiply*arrayValue,1);
 };
 
 const power = function() {
