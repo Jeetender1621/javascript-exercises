212458921a41b92b9b844280072b908a98074835
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-20 18:23:32 +0530

    Add logic for factorial testcase

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -19,12 +19,18 @@ const multiply = function(mulArray) {
   return mulArray.reduce( (multiply,arrayValue) => multiply*arrayValue,1);
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	// return num1**num2;
+  return Math.pow(num1,num2);
+
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  for(let i = 1; i<= num; i++){
+    factorial*=i;
+  }
+	return factorial
 };
 
 // Do not edit below this line
