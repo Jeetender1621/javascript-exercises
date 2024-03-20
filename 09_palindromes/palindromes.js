6243c6176676ee31b05e14f9ee653462bea781de
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-20 19:20:50 +0530

    Add logic for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Since we only consider letters and numbers, create a variable containing all valid characters
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
 
+    const reversedStr = cleanedStr.split('').reverse().join(''); // Reverse the string
+    return cleanedStr === reversedStr; 
 };
 
 // Do not edit below this line
