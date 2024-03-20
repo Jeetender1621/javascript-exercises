484c1052273ab3ab04b9332510407566a5a71701
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-20 18:42:31 +0530

    Add logic for getTitles solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+   const newBooksArray = books.map((book) => book.title);
+   return newBooksArray;
 };
 
 // Do not edit below this line
