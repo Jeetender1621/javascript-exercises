908277dea3fa98dc643e4eef910088552018944b
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-03-21 13:57:58 +0530

    Add logic for finding the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+   let ageDesc = array.sort( (a,b) =>{
+       const lastOne = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth :  new Date().getFullYear() - a.yearOfBirth;
+       const nextOne = b.yearOfDeath? b.yearOfDeath - b.yearOfBirth :  new Date().getFullYear() - b.yearOfBirth ;
+       return lastOne - nextOne;
+   })
+   return ageDesc[ageDesc.length-1];
 };
 
 // Do not edit below this line
