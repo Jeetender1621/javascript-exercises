e0391428971962a87bbb8f9bef5e5e1ac2245310
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-05-02 12:08:42 +0530

    Modify repeatString changes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,9 @@
 const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'; // Check for negative num before looping
     let reptString = "";
-    for(let i= 1; i <= num; i++){
-        reptString+= string;
+    for (let i = 0; i < num; i++) { // Start loop from i = 0, and condition should be i < num
+        reptString += string;
     }
-    if(num <0) return 'ERROR';
     return reptString;
 };
 
